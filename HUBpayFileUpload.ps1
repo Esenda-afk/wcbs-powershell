449bf2d957af661811563d4d4dfd16636a452e10
Michael Jarvis <michael@esenda.com>
2024-04-16 12:09:54 +0100

    Add sentry exception capturing

--- HUBpayFileUpload.ps1
+++ HUBpayFileUpload.ps1
@@ -12,61 +12,87 @@ if (!(Test-Path "$logpath")) {
 
 Start-Transcript -Path "$logpath" -Append
 
-$limit = (Get-Date).AddMinutes(-15)
-$logpathlimit = (Get-Date).Addhours(-24)
-echo $limit
-$path = "$albacs"
-$Extension = "*"
-$uploaddate = Get-Date -Format "ddMMyyyy_HHmm"
+function Ensure-ModuleInstalled {
+    Param (
+        [string]$ModuleName
+    )
 
-
-$uploadedpath = "$path\uploaded"
-$archivedpath = "$path\archive"
-$logstore = "C:\HubPay\s3-logs\"
-$logarchive = "$logstore\archive"
-
-if (!(Test-Path "$logstore")) {
-    
-    New-Item -ItemType Directory -Path "$logstore"
+    Write-Host "Checking if $ModuleName is installed..."
+    if (-not (Get-Module -ListAvailable -Name $ModuleName)) {
+        Write-Host "$ModuleName is not installed. Attempting to install..."
+        Install-Module -Name $ModuleName -Scope CurrentUser -Repository PSGallery -Force
+        Write-Host "$ModuleName installed successfully."
+    } else {
+        Write-Host "$ModuleName is already installed."
+    }
+    Import-Module $ModuleName
 }
 
+Ensure-ModuleInstalled -ModuleName "Sentry"
+Import-Module Sentry
+Start-Sentry 'https://38b83915f24124ddae8196758e939802@o1174556.ingest.us.sentry.io/4507095445798912'
 
-if (!(Test-Path "$logarchive")) {
-    
-    New-Item -ItemType Directory -Path "$logarchive"
+try
+{
+    $limit = (Get-Date).AddMinutes(-15)
+    $logpathlimit = (Get-Date).Addhours(-24)
+    echo $limit
+    $path = "$albacs"
+    $Extension = "*"
+    $uploaddate = Get-Date -Format "ddMMyyyy_HHmm"
+
+
+    $uploadedpath = "$path\uploaded"
+    $archivedpath = "$path\archive"
+    $logstore = "C:\HubPay\s3-logs\"
+    $logarchive = "$logstore\archive"
+
+    if (!(Test-Path "$logstore")) {
+        
+        New-Item -ItemType Directory -Path "$logstore"
+    }
+
+
+    if (!(Test-Path "$logarchive")) {
+        
+        New-Item -ItemType Directory -Path "$logarchive"
+    }
+
+    $files = Get-ChildItem -Path $path -Filter $Extension -File -Force | Where-Object {$_.CreationTime -lt $limit}
+
+    foreach ($file in $files){
+        echo "aws-vault exec $awss3user -- aws s3 cp $path\$file $s3bucket"
+        aws-vault exec $awss3user -- aws s3 cp $path\$file $s3bucket 
+
+        $fileName = [System.IO.Path]::GetFileNameWithoutExtension($file)
+        $fileExtension = [System.IO.Path]::GetExtension($file)
+
+        # Construct the new file name with the $uploaddate before the extension
+        $newFileName = "{0} {1}{2}" -f $fileName, $uploaddate, $fileExtension
+        $destination = Join-Path $uploadedpath $newFileName
+        echo "This is file variable: $file"
+        echo "This is path variable: $path"
+        echo "This is uploaddate variable: $uploaddate"
+        echo "This is destination variable: $destination"
+        echo "move-item -path $file -Destination $destination"
+        move-item -path $file -Destination $destination
+    }
+
+    Stop-Transcript
+    $logFile = Get-ChildItem -Path $logpath -File | Where-Object {$_.CreationTime -lt $logpathlimit}
+
+    if ($logFile) {
+        echo "This is the logfile variable: $logfile"
+        $archivefileName = [System.IO.Path]::GetFileNameWithoutExtension($logFile.FullName)
+        $archivefileExtension = [System.IO.Path]::GetExtension($logFile.FullName)
+        $archivelogName = "{0} {1}{2}" -f $archivefileName, $uploaddate, $archivefileExtension
+        copy-Item -Path $logFile.FullName -Destination "$logarchive\$archivelogName"
+        Set-Content -Path $logfile -Value ""
+    } else {
+        Write-Host "No files found older than 24 hours in $logpath."
+    }
 }
-
-$files = Get-ChildItem -Path $path -Filter $Extension -File -Force | Where-Object {$_.CreationTime -lt $limit}
-
-foreach ($file in $files){
-    echo "aws-vault exec $awss3user -- aws s3 cp $path\$file $s3bucket"
-    aws-vault exec $awss3user -- aws s3 cp $path\$file $s3bucket 
-
-    $fileName = [System.IO.Path]::GetFileNameWithoutExtension($file)
-    $fileExtension = [System.IO.Path]::GetExtension($file)
-
-    # Construct the new file name with the $uploaddate before the extension
-    $newFileName = "{0} {1}{2}" -f $fileName, $uploaddate, $fileExtension
-    $destination = Join-Path $uploadedpath $newFileName
-    echo "This is file variable: $file"
-    echo "This is path variable: $path"
-    echo "This is uploaddate variable: $uploaddate"
-    echo "This is destination variable: $destination"
-    echo "move-item -path $file -Destination $destination"
-    move-item -path $file -Destination $destination
+catch
+{
+    $_ | Out-Sentry
 }
-
-Stop-Transcript
-$logFile = Get-ChildItem -Path $logpath -File | Where-Object {$_.CreationTime -lt $logpathlimit}
-
-if ($logFile) {
-    echo "This is the logfile variable: $logfile"
-    $archivefileName = [System.IO.Path]::GetFileNameWithoutExtension($logFile.FullName)
-    $archivefileExtension = [System.IO.Path]::GetExtension($logFile.FullName)
-    $archivelogName = "{0} {1}{2}" -f $archivefileName, $uploaddate, $archivefileExtension
-    copy-Item -Path $logFile.FullName -Destination "$logarchive\$archivelogName"
-    Set-Content -Path $logfile -Value ""
-} else {
-    Write-Host "No files found older than 24 hours in $logpath."
-}
-
